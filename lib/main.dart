e6d568da40b482d93bffd23eda74ead8693b75d8
SriVinayA <srivinay.appari@gmail.com>
2019-05-29 19:03:42 +0530

    login page

--- lib/main.dart
+++ lib/main.dart
@@ -1,87 +1,64 @@
-import 'dart:async';
-
 import 'package:flutter/material.dart';
-import 'package:flutter/painting.dart';
 
-void main() => runApp(MaterialApp(
-      theme:
-          ThemeData(primaryColor: Colors.red, accentColor: Colors.yellowAccent),
-      debugShowCheckedModeBanner: false,
-      home: SplashScreen(),
-    ));
+void main() => runApp(MyApp());
 
-class SplashScreen extends StatefulWidget {
+class MyApp extends StatelessWidget {
   @override
-  _SplashScreenState createState() => _SplashScreenState();
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: LoginPage(),
+      theme: ThemeData(primarySwatch: Colors.blue),
+    );
+  }
 }
 
-class _SplashScreenState extends State<SplashScreen> {
+class LoginPage extends StatefulWidget {
   @override
-  void initState() {
-    // TODO: implement initState
-    super.initState();
-    Timer(Duration(seconds: 3), () => print("Splash Done"));
-  }
+  _LoginPageState createState() => _LoginPageState();
+}
 
+class _LoginPageState extends State<LoginPage>
+    with SingleTickerProviderStateMixin {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.greenAccent,
       body: Stack(
         fit: StackFit.expand,
         children: <Widget>[
-          Container(
-            decoration: BoxDecoration(color: Colors.redAccent),
+          Image(
+            image: AssetImage("assets/mario.png"),
+            fit: BoxFit.cover,
+            color: Colors.black87,
+            colorBlendMode: BlendMode.darken,
           ),
           Column(
-            mainAxisAlignment: MainAxisAlignment.start,
+            mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
-              Expanded(
-                flex: 2,
-                child: Container(
+              FlutterLogo(
+                size: 80,
+              ),
+              Form(
+                child: Theme(
+                  data: ThemeData(
+                      brightness: Brightness.dark, primarySwatch: Colors.teal),
                   child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
                     children: <Widget>[
-                      CircleAvatar(
-                        backgroundColor: Colors.white,
-                        radius: 50.0,
-                        child: Icon(
-                          Icons.shopping_cart,
-                          color: Colors.greenAccent,
-                          size: 50,
+                      TextFormField(
+                        decoration: InputDecoration(
+                          hintText: "Enter Email",
                         ),
+                        keyboardType: TextInputType.emailAddress,
                       ),
-                      Padding(
-                        padding: EdgeInsets.only(top: 10),
-                      ),
-                      Text(
-                        "ITM",
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 24,
-                          fontWeight: FontWeight.bold,
+                      TextFormField(
+                        decoration: InputDecoration(
+                          hintText: "Enter Password",
                         ),
-                      )
+                        keyboardType: TextInputType.text,
+                        obscureText: true,
+                      ),
                     ],
                   ),
-                ),
-              ),
-              Expanded(
-                flex: 1,
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: <Widget>[
-                    CircularProgressIndicator(),
-                    Padding(
-                      padding: EdgeInsets.only(top: 20),
-                    ),
-                    Text(
-                      "Business School",
-                      style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 18,
-                          fontWeight: FontWeight.bold),
-                    )
-                  ],
                 ),
               )
             ],
